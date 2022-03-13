8514f106063a40e6ef3b66d1bd8cf5b26102cb9f
Yonathan Assefa <yab95assefa@gmail.com>
2022-03-13 15:37:49 +0300

    [TEST] Models register to the admin site
    The models were registered to the admin for test. The models are registered with a custom configuration by using custom class based code.

--- FileSystem/a_users/admin.py
+++ FileSystem/a_users/admin.py
@@ -1,9 +1,39 @@
+from re import A
 from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
 from .models import Department, School
 
 User = get_user_model()
 
-admin.site.register((User, School, Department))
+class UserAdmin(auth_admin.UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (_('work info'), {'fields': ('department', 'type')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    list_display = ('username', 'email', 'department', 'type')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    search_fields = ('username', 'first_name', 'last_name', 'email')
+    ordering = ('username',)
+
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('department', 'school')
+    list_filter = ('school',)
+    search_fields = ('school', 'department')
+    ordering = ('school', 'department')
+
+class SchoolAdmin(admin.ModelAdmin):
+    list_display = ('school',)
+    list_filter = ('school',)
+    search_fields = ('school',)
+    ordering = ('school',)  
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Department, DepartmentAdmin)
+admin.site.register(School, SchoolAdmin)
