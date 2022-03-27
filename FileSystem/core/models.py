639bcc9d54070ad6c83013f0a342f6c504484221
Yonathan Assefa <yab95assefa@gmail.com>
2022-03-27 22:53:37 +0300

    [TEST] Document Model Added
    The document model is added. This model will be used to store the pdf files. the images that will be uploaded will be handled on the forms filed. Next, file sharing is done with DRY rules as much as possible by using room concept.

--- FileSystem/core/models.py
+++ FileSystem/core/models.py
@@ -3,12 +3,34 @@ from django.db import models
 # Create your models here.
 
 
-
-class File(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Document(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
     file = models.FileField(upload_to='files/')
+    cover = models.ImageField(upload_to='files/covers', null=True, blank=True)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, null=True, blank=True)
+    reply = models.BooleanField(default=False)
+    important = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
+    
     def __str__(self):
         return self.name
+
+
+class DocumentComment(models.Model):
+    document = models.ForeignKey('Document', on_delete=models.CASCADE)
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.comment
