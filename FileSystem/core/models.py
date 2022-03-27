5a73bd7130c9f8b4e5f6c82da9a88b3415f78537
Yonathan Assefa <yab95assefa@gmail.com>
2022-03-27 18:07:19 +0300

    [TEST] Added mock file model created
    Mock File model created to bring the idea, the file model was written by github copilot.

--- FileSystem/core/models.py
+++ FileSystem/core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class File(models.Model):
+    name = models.CharField(max_length=255)
+    file = models.FileField(upload_to='files/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
