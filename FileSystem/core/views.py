1ed4c5c16d5c644f216eabc9b8042732d3e7701f
Yonathan Assefa <yab95assefa@gmail.com>
2022-03-29 20:06:10 +0300

    [FIX] Multiple Image extension handler added
    Since the system is file management system and most of the time accepts A4 papers, it always rotates the picture 90 degrees if it's captured while rotated [Fixing it to fit A4]. And 3 file types will be handled [.jpg, .png, .jpeg]

--- FileSystem/core/views.py
+++ FileSystem/core/views.py
@@ -1,10 +1,9 @@
 import os
-from pydoc import Doc
-from django.shortcuts import render
 from fpdf import FPDF
 from .forms import DocumentForm
 from django.views.generic.edit import FormView
 from tempfile import NamedTemporaryFile
+from PIL import Image
 
 def pdfMaker(images):
     print("I am here")
@@ -12,8 +11,13 @@ def pdfMaker(images):
     pdf.add_page()
     pdf.set_font("Arial", size=12)
     for image in images:
-        pdf.image(image, 0,0,w=210, h=297)
-        pdf.add_page()
+        cover = Image.open(image)
+        X, Y = cover.size
+        if X > Y:
+            cover = cover.rotate(-90, expand=True)  
+            cover.save(image)
+        pdf.image(image, 0,0,w=210)
+        pdf.add_page()  
     pdf.output("test.pdf", "F")
 
 class DocumentFormView(FormView):
@@ -23,14 +27,16 @@ class DocumentFormView(FormView):
     def post(self, request, *args, **kwargs):
         form=DocumentForm(request.POST or None,request.FILES or None)
         files = request.FILES.getlist('image')
-        print(files)
+        extentions = ('jpg', 'png', 'jpeg')
         if form.is_valid():
             fls = []
             for file in files:
-                newfile = NamedTemporaryFile(delete=False, suffix='.png')
-                newfile.write(file.read())
-                fls.append(newfile.name)
-                newfile.close()
+                ext = file.name.split('.')[-1]
+                if ext.lower() in extentions:
+                    newfile = NamedTemporaryFile(delete=False, suffix='.'+ext)
+                    newfile.write(file.read())
+                    fls.append(newfile.name)
+                    newfile.close()
 
             pdfMaker(fls)
             # delete the temporary files
